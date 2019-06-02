3d7c31991905ab52b961eb0a6c1db8ad41adeddc
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-02 14:43:16 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,25 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     Show.maximum(:rating)
   end
+
+  def self.most_popular_show
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+  end
+
+  def self.ratings_sum
+    Show.sum(:ratings)
+  end
+
+  def self.popular_shows
+    Show.where("rating > ?" 5)
+  end
+
+  def self.shows_by_alphabetical_order
+  end
 end
