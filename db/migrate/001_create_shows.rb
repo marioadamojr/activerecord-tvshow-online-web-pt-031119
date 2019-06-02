a178eb512a144e6c6bcf67c932d5428b7f623c4c
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-02 14:33:36 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,4 +1,10 @@
 class 001CreateShows.rb < ActiveRecord::Migration[5.2]
   def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
   end
 end
